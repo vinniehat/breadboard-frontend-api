665870ba0e4f17d839fec0808aa3b14dd8a6f9e6
Vincent Lauro <vinniehat@gmail.com>
2020-10-23 19:09:54 -0400

    Init Vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SingleLED></SingleLED>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SingleLED from './components/Single LED.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SingleLED
   }
 }
 </script>
